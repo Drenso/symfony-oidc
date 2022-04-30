14687d46ea7ea63567e97bd382b5b84b1b5473c8
Bob van de Vijver <bob@drenso.nl>
2022-04-30 14:06:38 +0200

    Update to latest Rector, and rework configuration

--- rector.php
+++ rector.php
@@ -2,15 +2,11 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-  // Get parameters
-  $parameters = $containerConfigurator->parameters();
-  $parameters
-      ->set(Option::PATHS, [__DIR__ . '/src'])
-      ->set(Option::AUTO_IMPORT_NAMES, true)
-      ->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_80);
+return static function (RectorConfig $rc): void {
+  $rc->paths([__DIR__ . '/src']);
+  $rc->importNames();
+  $rc->phpVersion(PhpVersion::PHP_80);
 };
