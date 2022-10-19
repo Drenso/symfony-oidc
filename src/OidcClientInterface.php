1f448793d6cd63af2fc76b090b0860b89d3fd256
Bob van de Vijver <bob@drenso.nl>
2022-10-19 21:19:38 +0200

    Add missing $forceRememberMe parameter

--- src/OidcClientInterface.php
+++ src/OidcClientInterface.php
@@ -32,16 +32,22 @@ interface OidcClientInterface
   /**
    * Create the redirect that should be followed in order to authorize.
    *
-   * @param string|null $prompt One of 'none', 'login', 'consent', 'select_account' or 'create'
-   *                            If null or not supplied, the parameter will be omitted from the request
-   *                            Note that 'create' is currently in draft and might not be supported by every implementation
-   * @param string[]    $scopes An array of scopes to request
-   *                            If not supplied it will default to openid
+   * @param string|null $prompt          One of 'none', 'login', 'consent', 'select_account' or 'create'
+   *                                     If null or not supplied, the parameter will be omitted from the request
+   *                                     Note that 'create' is currently in draft and might not be supported by every implementation
+   * @param string[]    $scopes          An array of scopes to request
+   *                                     If not supplied it will default to openid
+   * @param bool        $forceRememberMe When set, the "remember me" security trigger will be force regardless of the
+   *                                     presence of the "remember me" parameter in the current request
    *
    * @throws OidcConfigurationException
    * @throws OidcConfigurationResolveException
    */
-  public function generateAuthorizationRedirect(?string $prompt = null, array $scopes = ['openid']): RedirectResponse;
+  public function generateAuthorizationRedirect(
+      ?string $prompt = null,
+      array $scopes = ['openid'],
+      bool $forceRememberMe = false,
+  ): RedirectResponse;
 
   /**
    * Retrieve the user information.
