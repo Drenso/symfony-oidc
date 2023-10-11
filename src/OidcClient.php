dc8618fdb80c1c804fa476876991b2e5530f98ea
LorisZ <loris.zanella@mail.com>
2023-10-11 15:32:46 +0200

    Also unset client-id parameter when doing basic auth

--- src/OidcClient.php
+++ src/OidcClient.php
@@ -411,6 +411,7 @@ class OidcClient implements OidcClientInterface
     $headers = [];
     if (in_array('client_secret_basic', $this->getTokenEndpointAuthMethods())) {
       $headers = ['Authorization: Basic ' . base64_encode(urlencode($this->clientId) . ':' . urlencode($this->clientSecret))];
+      unset($params['client_id']);
       unset($params['client_secret']);
     }
 
