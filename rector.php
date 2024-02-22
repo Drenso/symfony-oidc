b7eda9d42d6a9fc0615ae6bcbe4d968b04e11213
Tobias Feijten <tobias93@live.nl>
2024-02-22 09:57:54 +0100

    Update Rector config

--- rector.php
+++ rector.php
@@ -2,22 +2,20 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\PostRector\Rector\NameImportingPostRector;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $rc): void {
-  $rc->paths(['./src']);
-  $rc->importNames();
-  $rc->phpVersion(PhpVersion::PHP_80);
-  $rc->skip([
+return RectorConfig::configure()
+  ->withCache('./var/cache/rector', FileCacheStorage::class)
+  ->withPaths(['./src'])
+  ->withParallel(timeoutSeconds: 180, jobSize: 10)
+  ->withImportNames()
+  ->withPhpSets()
+  ->withSkip([
     JsonThrowOnErrorRector::class,
     NameImportingPostRector::class => [
       './src/OidcClient.php',
     ],
   ]);
-
-  $rc->import(LevelSetList::UP_TO_PHP_80);
-};
